5db629ca50b8a87d706ec1d23f9d6417696bb875
shinobig <shinobigakiya@gmail.com>
2020-12-13 14:39:49 -0600

    Se agrega file pra leer

--- src/com/shinobi/todolist/Controller.java
+++ src/com/shinobi/todolist/Controller.java
@@ -1,13 +1,18 @@
 package com.shinobi.todolist;
 
+import com.shinobi.todolist.datamodel.ToDoData;
 import com.shinobi.todolist.datamodel.ToDoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,38 +26,61 @@ public class Controller {
     @FXML
     private TextArea itemDetailsTextArea;
 
+    @FXML
+    private Label deadlineLabel;
+
     // Initialize Controller
     public void initialize() {
-        ToDoItem item1 = new ToDoItem("Mail Birthday Card", "Buy birthday card for Maria", LocalDate.of(2021,
-                Month.APRIL, 25));
-        ToDoItem item2 = new ToDoItem("Eat poop", "Eat lots of poop", LocalDate.of(2021,
-                Month.APRIL, 25));
-        ToDoItem item3 = new ToDoItem("Go to bathroom", "Never forget Bathroom", LocalDate.of(2021,
-                Month.MAY, 25));
-        ToDoItem item4 = new ToDoItem("Kill everyone", "Never forget, never forgive", LocalDate.of(2021,
-                Month.JUNE, 25));
+//        ToDoItem item1 = new ToDoItem("Mail Birthday Card", "Buy birthday card for Maria", LocalDate.of(2021,
+//                Month.APRIL, 25));
+//        ToDoItem item2 = new ToDoItem("Eat poop", "Eat lots of poop", LocalDate.of(2021,
+//                Month.APRIL, 25));
+//        ToDoItem item3 = new ToDoItem("Go to bathroom", "Never forget Bathroom", LocalDate.of(2021,
+//                Month.MAY, 25));
+//        ToDoItem item4 = new ToDoItem("Kill everyone", "Never forget, never forgive", LocalDate.of(2021,
+//               Month.JUNE, 25));
+//
+//      toDoItems = new ArrayList<ToDoItem>();
+//
+//        toDoItems.add(item1);
+//        toDoItems.add(item2);
+//        toDoItems.add(item3);
+//        toDoItems.add(item4);
+//
+//
+//         ToDoData.getInstance().setToDoItems(toDoItems);
 
-        toDoItems = new ArrayList<ToDoItem>();
 
-        toDoItems.add(item1);
-        toDoItems.add(item2);
-        toDoItems.add(item3);
-        toDoItems.add(item4);
 
-        toDoListView.getItems().setAll(toDoItems);
+        toDoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ToDoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends ToDoItem> observable, ToDoItem oldValue, ToDoItem newValue) {
+                if (newValue != null) {
+                    ToDoItem item = toDoListView.getSelectionModel().getSelectedItem();
+                    itemDetailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadlineLabel.setText(df.format(item.getDeadline()));
+                    // System.out.println(item.getDetails());
+                }
+            }
+        });
+
+        toDoListView.getItems().setAll(ToDoData.getInstance().getToDoItems());
         toDoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-
+        toDoListView.getSelectionModel().selectFirst();
     }
 
     @FXML
     public void handleClickListView() {
         ToDoItem item = toDoListView.getSelectionModel().getSelectedItem();
-
+        itemDetailsTextArea.setText(item.getDetails());
+        deadlineLabel.setText(item.getDeadline().toString());
+        /*
         StringBuilder sb = new StringBuilder(item.getDetails());
         sb.append("\n\n\n\n");
         sb.append("Due: ");
         sb.append(item.getDeadline());
-        itemDetailsTextArea.setText(sb.toString());
+         */
     }
 
 }
--- src/com/shinobi/todolist/Main.java
+++ src/com/shinobi/todolist/Main.java
@@ -1,10 +1,13 @@
 package com.shinobi.todolist;
 
+import com.shinobi.todolist.datamodel.ToDoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -16,8 +19,25 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void init() throws Exception {
+        try{
+            ToDoData.getInstance().loadToDoItems();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try {
+            ToDoData.getInstance().storeToDoItems();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
